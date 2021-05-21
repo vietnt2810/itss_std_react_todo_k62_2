b6c2c4ffc218ea66dc684c7c7652203adaaad671
EC2 Default User <ec2-user@ip-172-31-81-58.ec2.internal>
2021-05-21 10:25:58 +0000

    Fix

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 /* 
   【Todoのデータ構成】
@@ -8,38 +8,62 @@ import React, { useState } from 'react';
 */
 
 /* コンポーネント */
-import TodoItem from './TodoItem';
-import Input from './Input';
-import Filter from './Filter';
+import TodoItem from "./TodoItem";
+import Input from "./Input";
+import Filter from "./Filter";
 
 /* カスタムフック */
-import useStorage from '../hooks/storage';
+import useStorage from "../hooks/storage";
 
 /* ライブラリ */
-import {getKey} from "../lib/util";
+import { getKey } from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  const [items, putItems, clearItems] = useStorage();
+  /* テストコード 開始 */
+  //   { key: getKey(), text: "日本語の宿題", done: false },
+  //   { key: getKey(), text: "reactを勉強する", done: false },
+  //   { key: getKey(), text: "明日の準備をする", done: false },
+  //   /* テストコード 終了 */
+  // ]);
+
+  const [filter, setFilter] = React.useState("ALL");
+
+  const displayItems = items.filter((item) => {
+    if (filter === "ALL") return true;
+    if (filter === "TODO") return !item.done;
+    if (filter === "DONE") return item.done;
+  });
+
+  const handleCheck = (checked) => {
+    const newItems = items.map((item) => {
+      if (item.key === checked.key) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    putItems(newItems);
+  };
+
+  const handleAdd = (text) => {
+    putItems([...items, { key: getKey(), text, done: false }]);
+  };
+
+  const handleFilterChange = (value) => setFilter(value);
 
   return (
     <div className="panel">
-      <div className="panel-heading">
-        ITSS ToDoアプリ
-      </div>
-      {items.map(item => (
-        <label className="panel-block">
-            <input type="checkbox" />
-            {item.text}
-        </label>
+      <div className="panel-heading">ITSS ToDoアプリ</div>
+      <Input onAdd={handleAdd} />
+      <Filter onChange={handleFilterChange} value={filter} />
+      {displayItems.map((item) => (
+        <TodoItem key={item.key} item={item} onClick={handleCheck} />
       ))}
+      <div className="panel-block">{displayItems.length} items</div>
       <div className="panel-block">
-        {items.length} items
+        <button className="button is-light is-fullwidth" onClick={clearItems}>
+          全てのToDoを削除
+        </button>
       </div>
     </div>
   );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,9 +4,16 @@
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
-function TodoItem(  ) {
+function TodoItem({ item, onClick }) {
+  const handleChange = () => {
+    onClick(item);
+  };
   return (
     <label className="panel-block">
+      <input type="checkbox" checked={item.done} onChange={handleChange} />
+      <span className={item.done ? "has-text-grey-light" : ""}>
+        {item.text}
+      </span>
     </label>
   );
 }
